a35dafdfed52261de07f72a05dbd7da35eb6e88d
Nguyen Minh <enqminh@gmail.com>
2023-07-09 00:21:32 +0700

    update linear gradient color

--- src/components/AskChart.js
+++ src/components/AskChart.js
@@ -22,15 +22,28 @@ export default class AskChart extends PureComponent {
             right: 0,
             bottom: 0,
           }}
-          barCategoryGap="100%"
+          barCategoryGap="-1"
         >
+          <defs>
+            <linearGradient id="colorAsk" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="10%" stopColor="#d32f2f" />
+              <stop offset="20%" stopColor="#bf322e" />
+              <stop offset="30%" stopColor="#ac342e" />
+              <stop offset="40%" stopColor="#99342d" />
+              <stop offset="50%" stopColor="#86342d" />
+              <stop offset="60%" stopColor="#73332c" />
+              <stop offset="70%" stopColor="#61312b" />
+              <stop offset="80%" stopColor="#4e2f2a" />
+              <stop offset="90%" stopColor="#3c2c29" />
+              <stop offset="100%" stopColor="#282828" />
+            </linearGradient>
+          </defs>
           <XAxis dataKey="price" fontSize={14} reversed interval={0} />
           <YAxis hide tick={{ fontSize: 14 }} domain={[0, 3000]} />
           <Tooltip content={<CustomTooltip side="Asks" />} cursor={false} />
           <Bar
             dataKey="vol"
-            fill="#9e3538"
-            stroke="#9e3538"
+            fill="url(#colorAsk)"
             animationBegin={400}
             animationDuration={400}
             animationEasing="linear"
--- src/components/BidChart.js
+++ src/components/BidChart.js
@@ -22,15 +22,28 @@ export default class BidChart extends PureComponent {
             left: 0,
             bottom: 0,
           }}
-          barCategoryGap="100%"
+          barCategoryGap="-1"
         >
+          <defs>
+            <linearGradient id="colorBid" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="10%" stopColor="#1aa67c" />
+              <stop offset="20%" stopColor="#259772" />
+              <stop offset="30%" stopColor="#2b8868" />
+              <stop offset="40%" stopColor="#2e795e" />
+              <stop offset="50%" stopColor="#306b55" />
+              <stop offset="60%" stopColor="#305d4b" />
+              <stop offset="70%" stopColor="#304f42" />
+              <stop offset="80%" stopColor="#2e4239" />
+              <stop offset="90%" stopColor="#2b3531" />
+              <stop offset="100%" stopColor="#282828" />
+            </linearGradient>
+          </defs>
           <XAxis dataKey="price" fontSize={14} />
           <YAxis fontSize={14} domain={[0, 3000]} />
           <Tooltip content={<CustomTooltip side="Bids" />} cursor={false} />
           <Bar
             dataKey="vol"
-            fill="#1aa67c"
-            stroke="#1aa67c"
+            fill="url(#colorBid)"
             animationBegin={400}
             animationDuration={400}
             animationEasing="linear"
